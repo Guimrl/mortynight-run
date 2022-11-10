0faae2c9f263294a7a081b66b37e4e9cfd754196
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-10 11:09:26 -0300

    criando posicoes randomicas

--- game.js
+++ game.js
@@ -8,4 +8,22 @@ function tamanhoTela() {
 }
 tamanhoTela();
 
+function posicaoRandom() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) -90;
 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX, posicaoY);
+
+    //criar o elemento html
+    var morty = document.createElement('img');
+    morty.src = 'img/morty.png';
+    morty.className = 'morty1';
+    morty.style.left = posicaoX + 'px';
+    morty.style.top = posicaoY + 'px';
+    morty.style.position = 'absolute';
+
+    document.body.appendChild(morty);
+}
