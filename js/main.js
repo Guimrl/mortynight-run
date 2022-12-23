8c62920421dfdd9e4b93e3271989598b14bf102a
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-23 15:09:17 -0300

    criando menu, tela de seleção, tela de jogo

--- js/main.js
+++ js/main.js
@@ -1,123 +1,149 @@
-let altura = 0;
-let largura = 0;
-let vidas = 1;
-let tempo = 45;
+const start_btn = document.getElementById('start-btn');
+const telas = document.querySelectorAll('.tela');
+const personagem_escolhido_btn = document.querySelectorAll('.personagem-escolhido-btn');
+const game_container = document.getElementById('game-container');
 
-let criaMortyTempo = 2000;
+start_btn.addEventListener('click', () => telas[0].classList.add('up'));
 
-var nivel = window.location.search;
-nivel = nivel.replace('?', '');
+personagem_escolhido_btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        personagem_escolhido = { src, alt }
+        telas[1].classList.add('up')
+        setTimeout(criaPersonagem, 1000)
+        iniciarJogo()
+    })
+})
 
-if(nivel === 'morty') {
-    //2000
-    criaMortyTempo = 2000;
-} else if(nivel === 'normal') {
-    //1500
-    criaMortyTempo = 1500;
-} else if(nivel === 'hard') {
-    //1000
-    criaMortyTempo = 1000;
-} else if(nivel === 'extreme') {
-    //750
-    criaMortyTempo = 750;
-} else if(nivel === 'impossible') {
-    //500
-    criaMortyTempo = 500;
+function iniciarJogo() {
+    setInterval(aumentarTempo, 1000);
 }
 
-function tamanhoTela() {
-    altura = window.innerHeight;
-    largura = window.innerWidth;
 
-    console.log(largura, altura);
-}
-tamanhoTela();
 
-//cronometro
-let cronometro = setInterval(function() {
 
-    tempo -= 1;
-    if(tempo < 0) {
-        //vitoria
-        clearInterval(cronometro);
-        clearInterval(criaMorty);
-        window.location.href = 'vitoria.html';
-    } else {
-        document.getElementById('cronometro').innerHTML = tempo;
-        // console.log(cronometro)
-    }
-},1000)
+// let altura = 0;
+// let largura = 0;
+// let vidas = 1;
+// let tempo = 45;
 
-function posicaoRandomica() {
+// let criaMortyTempo = 2000;
 
-    //remover o morty anterior(caso exista)
-    let criaMorty = document.getElementById('morty');
-    if(criaMorty) {
-        criaMorty.remove();
+// var nivel = window.location.search;
+// nivel = nivel.replace('?', '');
 
-        //gameover
-        if(vidas > 3) {
-            window.location.href = 'fim_de_jogo.html';
-            alert('oi')
-        } else {
-            //controle de vidas
-            document.getElementById('v' + vidas).src='img/coracao_vazio.png';
-            vidas++;
-        }
-    }
+// if(nivel === 'morty') {
+//     //2000
+//     criaMortyTempo = 2000;
+// } else if(nivel === 'normal') {
+//     //1500
+//     criaMortyTempo = 1500;
+// } else if(nivel === 'hard') {
+//     //1000
+//     criaMortyTempo = 1000;
+// } else if(nivel === 'extreme') {
+//     //750
+//     criaMortyTempo = 750;
+// } else if(nivel === 'impossible') {
+//     //500
+//     criaMortyTempo = 500;
+// }
+
+// function tamanhoTela() {
+//     altura = window.innerHeight;
+//     largura = window.innerWidth;
+
+//     console.log(largura, altura);
+// }
+// tamanhoTela();
+
+// //cronometro
+// let cronometro = setInterval(function() {
+
+//     tempo -= 1;
+//     if(tempo < 0) {
+//         //vitoria
+//         clearInterval(cronometro);
+//         clearInterval(criaMorty);
+//         window.location.href = 'vitoria.html';
+//     } else {
+//         document.getElementById('cronometro').innerHTML = tempo;
+//         // console.log(cronometro)
+//     }
+// },1000)
+
+// function posicaoRandomica() {
+
+//     //remover o morty anterior(caso exista)
+//     let criaMorty = document.getElementById('morty');
+//     if(criaMorty) {
+//         criaMorty.remove();
+
+//         //gameover
+//         if(vidas > 3) {
+//             window.location.href = 'fim_de_jogo.html';
+//             alert('oi')
+//         } else {
+//             //controle de vidas
+//             document.getElementById('v' + vidas).src='img/coracao_vazio.png';
+//             vidas++;
+//         }
+//     }
 
     
 
-    //sorteia uma posicao randomica na tela
-    let posicaoX = Math.floor(Math.random() * largura) - 90;
-    let posicaoY = Math.floor(Math.random() * altura) - 90;
+//     //sorteia uma posicao randomica na tela
+//     let posicaoX = Math.floor(Math.random() * largura) - 90;
+//     let posicaoY = Math.floor(Math.random() * altura) - 90;
 
-    posicaoX = posicaoX < 0 ? 0 : posicaoX;
-    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+//     posicaoX = posicaoX < 0 ? 0 : posicaoX;
+//     posicaoY = posicaoY < 0 ? 0 : posicaoY;
     
-    console.log(posicaoX, posicaoY);
+//     console.log(posicaoX, posicaoY);
     
-    //criar morty elemento html
-    let morty = document.createElement('img');
-    morty.src = 'img/morty.png';
-    morty.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
-    morty.style.left = posicaoX + 'px';
-    morty.style.top = posicaoY + 'px';
-    morty.style.position = 'absolute';
+//     //criar morty elemento html
+//     let morty = document.createElement('img');
+//     morty.src = 'img/morty.png';
+//     morty.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+//     morty.style.left = posicaoX + 'px';
+//     morty.style.top = posicaoY + 'px';
+//     morty.style.position = 'absolute';
 
-    morty.id = 'morty';
+//     morty.id = 'morty';
 
-    //controle dos clicks dos mortys
-    morty.onclick = function() {
-        this.remove();
-    }
+//     //controle dos clicks dos mortys
+//     morty.onclick = function() {
+//         this.remove();
+//     }
 
-    document.body.appendChild(morty);
+//     document.body.appendChild(morty);
 
-}
+// }
 
-//gera tamanhos aleatorios de mortys
-function tamanhoAleatorio() {
-    let classe = Math.floor(Math.random() * 3);
+// //gera tamanhos aleatorios de mortys
+// function tamanhoAleatorio() {
+//     let classe = Math.floor(Math.random() * 3);
     
-    switch(classe) {
-        case 0:
-            return 'morty1';
-        case 1:
-            return 'morty2';
-        case 2:
-            return 'morty3';
-    }
-}
+//     switch(classe) {
+//         case 0:
+//             return 'morty1';
+//         case 1:
+//             return 'morty2';
+//         case 2:
+//             return 'morty3';
+//     }
+// }
 
-//gera tamanhos de mortys aleatorios
-function ladoAleatorio() {
-    let classe = Math.floor(Math.random() * 2);
+// //gera tamanhos de mortys aleatorios
+// function ladoAleatorio() {
+//     let classe = Math.floor(Math.random() * 2);
     
-    switch(classe) {
-        case 0:
-            return 'ladoA';
-        case 1:
-            return 'ladoB';
-    }
-}
+//     switch(classe) {
+//         case 0:
+//             return 'ladoA';
+//         case 1:
+//             return 'ladoB';
+//     }
+// }
