4261ad998e8274e4275e1a41c2d43c498e028da2
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-23 15:30:23 -0300

    criando cronometro e começando a instanciar personagens

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@ const start_btn = document.getElementById('start-btn');
 const telas = document.querySelectorAll('.tela');
 const personagem_escolhido_btn = document.querySelectorAll('.personagem-escolhido-btn');
 const game_container = document.getElementById('game-container');
+const tempoEl = document.getElementById('tempo');
+let segundos = 0;
+let personagem_escolhido = {}
 
 start_btn.addEventListener('click', () => telas[0].classList.add('up'));
 
 personagem_escolhido_btn.forEach(btn => {
     btn.addEventListener('click', () => {
-        const img = btn.querySelector('img')
-        const src = img.getAttribute('src')
-        const alt = img.getAttribute('alt')
-        personagem_escolhido = { src, alt }
-        telas[1].classList.add('up')
-        setTimeout(criaPersonagem, 1000)
-        iniciarJogo()
+        const img = btn.querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        personagem_escolhido = { src, alt };
+        telas[1].classList.add('up');
+        setTimeout(criaPersonagem, 1000);
+        iniciarJogo();
     })
 })
 
@@ -21,6 +24,40 @@ function iniciarJogo() {
     setInterval(aumentarTempo, 1000);
 }
 
+function aumentarTempo() {
+    let m = Math.floor(segundos / 60);
+    let s = segundos % 60;
+    m = m < 10 ? `0${m}` : m
+    s = s < 10 ? `0${s}` : s
+    tempoEl.innerHTML = `${m}:${s}`
+    segundos++;
+}
+
+function posicaoRandomica() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const x = Math.random() * (width - 200) + 100;
+    const y = Math.random() * (height - 200) + 100;
+    return { x, y }
+}
+
+function criaPersonagem() {
+    const personagem = document.createElement('div');
+    personagem.classList.add('personagem');
+    const { x, y } = posicaoRandomica();
+    personagem.style.top = `${y}px`
+    personagem.style.left = `${x}px`
+    personagem.innerHTML = `<img src="${personagem_escolhido.src}"
+    />`
+
+    personagem.addEventListener('click', clickPersonagem)
+
+    game_container.appendChild(personagem);
+}
+
+function adicionaPersonagem() {
+    setTimeout(criaPersonagem, 1000);
+}
 
 
 
